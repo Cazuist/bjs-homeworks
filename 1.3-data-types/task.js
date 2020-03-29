250184dccde400fd69c8e0369d926df8f3a7efdd
Cazuist <sovrus@mail.ru>
2020-03-29 18:39:29 +0300

    Dicision of task 1.3 Data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,17 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  let creditBody = amount - contribution;
+  let today = new Date();
+  let creditTerm = ( (date.getFullYear() - today.getFullYear() ) * 12) + date.getMonth() - today.getMonth();
+  let monthPercent = percent / 100 / 12;
+  let monthPayment = creditBody * (monthPercent + monthPercent/(((1+monthPercent) ** creditTerm) -1 ));
+  let totalAmount = +(monthPayment * creditTerm).toFixed(2);
+  
+  return totalAmount;
 }
 
-function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+function getGreeting(name) {  
+  return `Привет, мир! Меня зовут ${name === '' || name === null || name === undefined ? 'Аноним' : name}`;
 }
+
