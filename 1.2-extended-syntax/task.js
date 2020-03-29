49eaff88c369f20a151e4a5d049cbd836ddee019
Cazuist <sovrus@mail.ru>
2020-03-29 17:30:50 +0300

    Decision of task 1.2 Extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,39 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a, b, c){
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  let roots = [];
+
+  if (discriminant === 0) {
+  	roots.push(-b / 2 * a);
+  } else if (discriminant > 0) {
+    roots.push( (-b + Math.pow(discriminant, 0.5)) / 2 * a);
+    roots.push( (-b - Math.pow(discriminant, 0.5)) / 2 * a);
+  }
+
+  return roots; 
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+  let averageMark = 0;
+  let sum = 0;
+  let newList = marks.slice(0, 5);
+
+  if (marks.length > 0) {
+    for (let mark of newList) {
+      sum += mark;
+      averageMark = (sum / newList.length);
+    }
+  } else if (marks.length > 5) {
+    console.log('Получено больше 5 оценок!');
+  }
+
+  return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday){
+  let today = new Date();
+  let age = today.getFullYear() - dateOfBirthday.getFullYear();
+
+
+  return (age > 18) ? `Не желаете ли олд-фэшн, ${name}?` : 
+        `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
 }
