60055c08900a217ba2d063c56c8bb4657e2ca6b0
Cazuist <sovrus@mail.ru>
2020-03-31 16:26:08 +0300

    Decision of task 2.2 Object_prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,42 @@
+//задание 1
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+  const sound = animal ? animal.sound : null;
+  return sound;
 }
 
+//задание 2
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  let sum = 0;
+
+  for (let mark of marks) {
+    sum += +mark;
+  }
+
+  return Math.round( sum / marks.length ) || 0;
+}
+
+//задание 3
+function isLeapYear(year) {
+  return ( year % 400 === 0 || (year % 4 === 0 && year % 400 != 0) || year === 0 );
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  const now = Date.now();
+  const dayOfBirthday = ( new Date(birthday) ).getTime();
+
+  const msInYear = 365 * 24 * 60 * 60 * 1000;
+  const msInDay = 24 * 60 * 60 * 1000;
+  
+  const ageMS = now - dayOfBirthday;
+  const currentYear = ( new Date() ).getFullYear();
+  const birthdayYear = ( new Date(birthday) ).getFullYear();
+  let leapDays = 0;
+
+  for (let i = birthdayYear; i < currentYear; i++) {
+    if (isLeapYear(i)) {
+      ++leapDays;
+    }
+  }  
+  
+  return  (ageMS - leapDays * msInDay) / msInYear > 18;
 }
